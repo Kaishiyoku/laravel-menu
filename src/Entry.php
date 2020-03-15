11d2902b7ceeb574b56705e65e4fc5f6e05631ab
Kaishiyoku <dmp@nfze.de>
2020-03-15 21:21:13 +0100

    adjusted current route check

--- src/Entry.php
+++ src/Entry.php
@@ -3,6 +3,7 @@
 namespace Kaishiyoku\LaravelMenu;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Stringable;
 
 class Entry
 {
@@ -17,13 +18,20 @@ class Entry
     private $data;
 
     /**
+     * @var bool
+     */
+    private $strict;
+
+    /**
      * @param string $view
      * @param array $data
+     * @param bool $strict
      */
-    public function __construct(string $view, array $data)
+    public function __construct(string $view, array $data, bool $strict = false)
     {
         $this->view = $view;
         $this->data = collect($data);
+        $this->strict = $strict;
     }
 
     /**
@@ -31,8 +39,24 @@ class Entry
      */
     public function render(): string
     {
-        $this->data->put('isActive', Route::currentRouteName() === $this->data->get('route'));
+        $this->data->put('isActive', $this->isCurrentRoute());
 
         return view($this->view, $this->data->toArray());
+    }
+
+    /**
+     * @return bool
+     */
+    private function isCurrentRoute(): bool
+    {
+        $currentRouteName = Route::currentRouteName();
+        $routeName = $this->data->get('route');
+        $routeFn = $this->strict ? function ($name) { return $name; } : function ($name) {
+            return (new Stringable($name))
+                ->explode('.')
+                ->first();
+        };
+
+        return $routeFn($currentRouteName) === $routeFn($routeName);
     }
 }
